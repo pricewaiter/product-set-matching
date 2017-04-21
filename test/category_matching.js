b996fe535ff34b21ea3e63730fdf197f281ead4b
Mike Estes <mikejestes@gmail.com>
2017-04-21 15:37:43 -0700

    Add missing category test file.

---
+++ test/category_matching.js
@@ -0,0 +1,92 @@
+const { expect } = require('chai');
+
+const { categoryMatch } = require('../src/checks/category');
+
+describe('Category matching', () => {
+    const cellPhone = {
+        sku: 'cell-phone',
+        categories: [
+            'Electronics',
+            ['Electronics', 'Cell Phones'],
+            'Fire Sale',
+            ['Brands', 'Apple', 'iPhone'],
+        ],
+    };
+
+    const tests = [
+        {
+            desc: 'does not match invalidly formatted input',
+            rule: {
+                op: 'eq',
+                value: {},
+            },
+            product: cellPhone,
+            shouldMatch: false,
+        },
+        {
+            desc: 'matches single segment category',
+            rule: {
+                property: 'category',
+                op: 'eq',
+                value: [['Fire Sale']],
+            },
+            product: cellPhone,
+            shouldMatch: true,
+        },
+        {
+            desc: 'matches single segment category regardless of case',
+            rule: {
+                property: 'category',
+                op: 'eq',
+                value: [['FIRE Sale']],
+            },
+            product: cellPhone,
+            shouldMatch: true,
+        },
+        {
+            desc: 'matches multi-segment category',
+            rule: {
+                property: 'category',
+                op: 'eq',
+                value: [['Electronics', 'Cell Phones']],
+            },
+            product: cellPhone,
+            shouldMatch: true,
+        },
+        {
+            desc: 'does not match on parent category when not specified',
+            rule: {
+                property: 'category',
+                op: 'eq',
+                value: [['Brands', 'Apple']],
+            },
+            product: cellPhone,
+            shouldMatch: false,
+        },
+        {
+            desc: 'can be inverted',
+            rule: {
+                property: 'category',
+                op: 'noteq',
+                value: [['Electronics', 'Breathalyzers']],
+            },
+            product: cellPhone,
+            shouldMatch: true,
+        },
+    ];
+
+    tests.forEach(({ desc, rule, product, shouldMatch }) => {
+        if (!rule) {
+            it(desc);
+            return;
+        }
+
+        it(desc, () => {
+            const params = {
+                products: [product],
+            };
+            const matches = categoryMatch(params, rule);
+            expect(matches).to.equal(shouldMatch);
+        });
+    });
+});
